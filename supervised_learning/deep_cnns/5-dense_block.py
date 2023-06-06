9cb80fa7c79da8092725bd2a4bc61312f3d4d52f
jurgenTafaj <jurgen.tafaj@1up.al>
2023-06-06 03:41:05 -0700

    better scripts

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
+"""Deep neural architecture"""
+import tensorflow.keras as K
 
-densenet121 = __import__('7-densenet121').densenet121
 
-if __name__ == '__main__':
-    model = densenet121(32, 0.5)
-    model.summary()
+def dense_block(X, nb_filters, growth_rate, layers):
+    """Dense block"""
+    prev = X
+    for i in range(layers):
+        L = K.layers.BatchNormalization(axis=3)(prev)
+        L = K.layers.Activation('relu')(L)
+        L = K.layers.Conv2D(filters=growth_rate * 4,
+                            kernel_size=(1, 1),
+                            padding='same',
+                            strides=(1, 1),
+                            kernel_initializer='he_normal')(L)
+        L = K.layers.BatchNormalization(axis=3)(L)
+        L = K.layers.Activation('relu')(L)
+        L = K.layers.Conv2D(filters=growth_rate,
+                            kernel_size=(3, 3),
+                            padding='same',
+                            strides=(1, 1),
+                            kernel_initializer='he_normal')(L)
+        prev = K.layers.concatenate([prev, L], axis=3)
+        nb_filters = nb_filters + growth_rate
+    return(prev, nb_filters)
